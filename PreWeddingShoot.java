49df9301027844af90b3e65e6c21ac0bffe10845
sanjay8494 <113335608+sanjay8494@users.noreply.github.com>
2022-11-08 17:55:08 -0800

    main

---
+++ PreWeddingShoot.java
@@ -0,0 +1,19 @@
+package com.xworkz.Inheritance;
+
+public class PreWeddingShoot {
+	
+	public String boyName;
+	public String girlName;
+	public String Location;
+	
+	public PreWeddingShoot() {}
+	
+	public PreWeddingShoot(String boyName,String girl,String location)
+	{
+	super();
+	this.boyName=boyName;
+	this.girlName=girlName;
+    Location=location;
+	}
+
+}
---
+++ WeddingShoot.java
@@ -0,0 +1,11 @@
+package com.xworkz.Inheritance;
+
+public class WeddingShoot extends PreWeddingShoot {
+	
+	public  WeddingShoot(String boyName,String girlName,String location)
+	{
+		super(boyName,girlName,location);
+	}
+
+
+}
