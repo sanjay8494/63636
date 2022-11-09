aaae600d1a60a3be82541cab0f05c270072df5fd
sanjay8494 <113335608+sanjay8494@users.noreply.github.com>
2022-11-09 09:41:29 -0800

    main

---
+++ ShoppingMallRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.sanju;
+
+import com.xworkz.Encapsulation.Inheritance.Orion;
+
+public class ShoppingMallRunner {
+	
+
+	public static void main(String[] args) {
+		Orion orion=new  Orion( "Orian up town","blue",23,"madan",23);
+		orion.display();
+	}
+
+}
+
+
